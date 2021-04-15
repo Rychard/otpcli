98521f6fc2d6f499c7fd0204689d7af3d2b6b6c5
Joshua Shearer <rychard@users.noreply.github.com>
2021-04-15 11:48:27 -0400

    Errors should be handled gracefully

--- src/Program.cs
+++ src/Program.cs
@@ -8,26 +8,72 @@ namespace otpcli
     {
         private static String FileName = ".otpcli";
 
-        static void Main(string[] args)
+        static Int32 Main(string[] args)
         {
             String homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             String filePath = Path.Combine(homePath, FileName);
 
-            String base64Secret = String.Empty;
-            if(File.Exists(filePath))
+            if(!File.Exists(filePath))
             {
-                base64Secret = File.ReadAllText(filePath);
+                Console.Error.WriteLine($"Configuration file does not exist: {filePath}");
+                return 1;
             }
 
-            Byte[] secretKey = Base64Decode(base64Secret);
-            var totp = new Totp(secretKey);
-            Console.WriteLine(totp.ComputeTotp());
-        }
+            String base64Secret;
+            try
+            {
+                base64Secret = File.ReadAllText(filePath).Trim();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"Unable to read data from configuration file: {filePath}");
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
 
-        public static Byte[] Base64Decode(string base64EncodedData)
-        {
-            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-            return base64EncodedBytes;
+            if (String.IsNullOrWhiteSpace(base64Secret))
+            {
+                Console.Error.WriteLine($"Configuration file does not contain an encoded secret: {filePath}");
+                return 1;
+            }
+
+            Byte[] secretKey;
+            try
+            {
+                 secretKey = Convert.FromBase64String(base64Secret);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"An error occurred while decoding the secret key in the configuration file: {filePath}");
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
+
+            Totp totp;
+            try
+            {
+                totp = new Totp(secretKey);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"An error occurred while initializing the OTP library.");
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
+
+            try
+            {
+                String result = totp.ComputeTotp();
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"An error occurred while generating the TOTP code.");
+                Console.Error.WriteLine(ex.ToString());
+                return 1;
+            }
+
+            return 0;
         }
     }
 }
