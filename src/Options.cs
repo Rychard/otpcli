edfd9ee79c9afc1e247544b480fe1c961e82fe61
Joshua Shearer <rychard@users.noreply.github.com>
2021-08-18 11:59:56 -0400

    Allow for configuration via command line arguments

---
+++ src/Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommandLine;
+
+namespace otpcli
+{
+	class Options
+	{
+		[Option('c', "config", 
+			HelpText = "Use the specified configuration file.  If set to a non-absolute path, the file will be searched for using the current working directory first, and will fallback to the user's home directory.  If the file does not exist, the program will exit with an error code.")]
+		public String ConfigurationFile { get; set; }
+
+		[Option('w', "wait",
+			HelpText = "Wait for a fresh token before returning.")]
+		public Boolean Wait { get; set; }
+
+		[Option('v', "verbose",
+			HelpText = "Output will include informational messages.")]
+		public Boolean Verbose { get; set; }
+	}
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,50 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using CommandLine;
 using OtpNet;
 
 namespace otpcli
 {
     class Program
     {
-        private static String FileName = ".otpcli";
-
         static Int32 Main(string[] args)
         {
-            String homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            String filePath = Path.Combine(homePath, FileName);
+            return Parser.Default.ParseArguments<Options>(args)
+                .MapResult(
+                    options => RunOptions(options), 
+                    errors => HandleParseError(errors)
+                );
+        }
 
-            if(!File.Exists(filePath))
+        static String ResolveFile(String filename, Boolean verbose = false)
+        {
+            if(String.IsNullOrWhiteSpace(filename))
             {
-                Console.Error.WriteLine($"Configuration file does not exist: {filePath}");
-                return 1;
+                filename = ".otpcli";
             }
 
-            String base64Secret;
+            if(Path.IsPathFullyQualified(filename) && File.Exists(filename))
+            {
+                if (verbose) { Console.WriteLine($"Using fully qualified path: {filename}"); }
+                return filename;
+            }
+
+            String current;
+
+            // Search for the file in the current working directory
+            String currentWorkingDirectory = Directory.GetCurrentDirectory();
+            current = Path.Combine(currentWorkingDirectory, filename);
+            if (verbose) { Console.WriteLine($"Checking in current working directory: {current}"); }
+            if (File.Exists(current)) { return current; }
+            if (verbose) { Console.WriteLine($"File does not exist: {current}"); }
+
+            // Search for the file in the user's home directory
+            String homePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            current = Path.Combine(homePath, filename);
+            if (verbose) { Console.WriteLine($"Checking in user's home directory: {current}"); }
+            if (File.Exists(current)) { return current; }
+            if (verbose) { Console.WriteLine($"File does not exist: {current}"); }
+
+            // If the file is not found, return nothing.
+            if (verbose) { Console.WriteLine($"Configuration file not found!"); }
+            return String.Empty;
+        }
+
+        static Byte[] DecodeSecretKey(String input, bool verbose)
+        {
+            Byte[] decodedBytes = null;
             try
             {
-                base64Secret = File.ReadAllText(filePath).Trim();
+                var base32Bytes = Base32Encoding.ToBytes(input);
+                decodedBytes = base32Bytes;
+            }
+            catch (Exception) 
+            {
+                if (verbose) { Console.WriteLine($"Configuration file does not contain a valid Base32 value."); }
+            }
+
+            try
+            {
+                var base64Bytes = Convert.FromBase64String(input);
+                decodedBytes = base64Bytes;
+            }
+            catch (Exception) 
+            {
+                if (verbose) { Console.WriteLine($"Configuration file does not contain a valid Base64 value."); }
+            }
+
+            if(decodedBytes == null)
+            {
+                throw new InvalidDataException("The specified value is not an encoded Base32/Base64 string!");
+            }
+
+            return decodedBytes;
+        }
+
+        static Int32 RunOptions(Options options)
+        {
+            String configurationFile = ResolveFile(options.ConfigurationFile, verbose: options.Verbose);
+
+            String encodedSecret;
+            try
+            {
+                encodedSecret = File.ReadAllText(configurationFile).Trim();
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine($"Unable to read data from configuration file: {filePath}");
+                Console.Error.WriteLine($"Unable to read data from configuration file: {configurationFile}");
                 Console.Error.WriteLine(ex.ToString());
                 return 1;
             }
 
-            if (String.IsNullOrWhiteSpace(base64Secret))
-            {
-                Console.Error.WriteLine($"Configuration file does not contain an encoded secret: {filePath}");
-                return 1;
-            }
-
-            Byte[] secretKey;
+            Byte[] decodedSecret;
             try
             {
-                 secretKey = Convert.FromBase64String(base64Secret);
+                decodedSecret = DecodeSecretKey(encodedSecret, verbose: options.Verbose);
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine($"An error occurred while decoding the secret key in the configuration file: {filePath}");
+                Console.Error.WriteLine($"An error occurred while decoding the secret key in the configuration file: {configurationFile}");
                 Console.Error.WriteLine(ex.ToString());
                 return 1;
             }
@@ -52,7 +113,7 @@ namespace otpcli
             Totp totp;
             try
             {
-                totp = new Totp(secretKey);
+                totp = new Totp(decodedSecret);
             }
             catch (Exception ex)
             {
@@ -63,8 +124,26 @@ namespace otpcli
 
             try
             {
-                String result = totp.ComputeTotp();
-                Console.WriteLine(result);
+                String token = totp.ComputeTotp();
+                if (options.Wait) 
+                {
+                    if (options.Verbose)
+                    {
+                        Console.WriteLine($"Current token: {token}");
+                    }
+
+                    // If the user wants to ensure a fresh token is returned, we must wait for the next code.
+                    Int32 remainingSeconds = totp.RemainingSeconds();
+                    token = totp.ComputeTotp(DateTime.Now.AddSeconds(remainingSeconds));
+
+                    if (options.Verbose)
+                    {
+                        Console.WriteLine($"The next token will become valid in {remainingSeconds} seconds");
+                        Console.WriteLine($"Next token: {token}");
+                    }
+                    System.Threading.Thread.Sleep(remainingSeconds * 1000);
+                }
+                Console.WriteLine(token);
             }
             catch (Exception ex)
             {
@@ -74,6 +153,12 @@ namespace otpcli
             }
 
             return 0;
+        }
+
+        static Int32 HandleParseError(IEnumerable<Error> errs)
+        {
+            errs.Output();
+            return 1;
         }
     }
 }
